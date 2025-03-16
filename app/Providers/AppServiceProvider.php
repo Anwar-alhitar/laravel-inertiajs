e4d44e57a80f2f79913ea567d97e7b37827a8884
anwar_Alhitar <anwaralhitar@gmail.com>
2025-03-16 23:52:50 +0300

    skiping tge length of the field

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
-        Schema::defaultStringLength(191);
+        Schema::defaultStringLength(191);  // for skiping field length
 
     }
 }
