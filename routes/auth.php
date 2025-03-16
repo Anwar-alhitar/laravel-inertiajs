0520e03b1453236d09c58cf2fd45f07330122a8d
anwar_Alhitar <anwaralhitar@gmail.com>
2025-03-16 23:51:22 +0300

    inerta auth routing

--- routes/auth.php
+++ routes/auth.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// routes provided by inertia when install breez
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
